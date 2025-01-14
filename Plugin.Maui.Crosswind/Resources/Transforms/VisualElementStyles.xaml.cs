d0c1d0c1ee58dc58d78b04a0f174d867052f304f
Steven Thewissen <github@thewissen.io>
2025-01-14 15:39:34 +0100

    Create VisualElementStyles.xaml.cs

---
+++ Plugin.Maui.Crosswind/Resources/Transforms/VisualElementStyles.xaml.cs
@@ -0,0 +1,9 @@
+namespace Plugin.Maui.Crosswind.Resources.Transforms;
+
+public partial class VisualElementStyles : ResourceDictionary
+{
+    public VisualElementStyles()
+    {
+        InitializeComponent();
+    }
+}
